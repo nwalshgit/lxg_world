312d82321fbe4c2db9b4d9f2088a0b06e4a3341f
Nathan Walsh <16801436+nwalshgit@users.noreply.github.com>
2022-12-01 08:48:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,48 +1,16 @@
 from setuptools import setup, find_packages
 
-
-def install_deps():
-    """Reads requirements.txt and preprocess it
-    to be feed into setuptools.
-
-    This is the only possible way (we found)
-    how requirements.txt can be reused in setup.py
-    using dependencies from private github repositories.
-
-    Links must be appendend by `-{StringWithAtLeastOneNumber}`
-    or something like that, so e.g. `-9231` works as well as
-    `1.1.0`. This is ignored by the setuptools, but has to be there.
-
-    Warnings:
-        to make pip respect the links, you have to use
-        `--process-dependency-links` switch. So e.g.:
-        `pip install --process-dependency-links {git-url}`
-
-    Returns:
-         list of packages and dependency links.
-    """
-    default = open('requirements.txt', 'r').readlines()
-    new_pkgs = []
-    links = []
-    for resource in default:
-        if 'git+ssh' in resource:
-            pkg = resource.split('#')[-1]
-            # Why not just add version to requirements
-            links.append(resource.strip() + '-9876543210')
-            new_pkgs.append(pkg.replace('egg=', '').rstrip())
-        else:
-            new_pkgs.append(resource.strip())
-    return new_pkgs, links
-
-pkgs, new_links = install_deps()
-
 setup(
-    # install_requires=[
-    #    "lxg-world==1.0",
-    # ],
-    # dependency_links=[
-    #     "https://github.com/dmfigol/smartsheet-python.sdk/archive/no-setuptools-scm.zip#egg=smartsheet-python-sdk-10.1.3.3"
-    # ]
-    install_requires=pkgs,
-    dependency_links=new_links,
+    name='lxg_world',
+    version='0.0.2',
+    description='A small example package',
+    url='https://github.com/nwalshgit/lxg_world',
+    author='Nathan Walsh <nwalsh@example.com>',
+    #packages=['lxg_world'],
+    install_requires=[
+    #   "lxg-world==1.0",
+    ],
+    dependency_links=[
+    #    "https://github.com/dmfigol/smartsheet-python.sdk/archive/no-setuptools-scm.zip#egg=smartsheet-python-sdk-10.1.3.3"
+    ]
 )
